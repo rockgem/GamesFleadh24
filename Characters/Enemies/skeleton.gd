51adf3c7e97576fa832c0d83a0d0044571656c5c
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2024-02-24 20:53:57 +0000

    Animation somewhat implemented but animation being triggered in a funny way

--- Characters/Enemies/skeleton.gd
+++ Characters/Enemies/skeleton.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-var speed = 25
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+var speed = 10
 var player = null
 var chase = false
+
+
+
 func _physics_process(delta):
 	if chase == true:
 		position += (player.position - position)/speed
-	
+		
 
 func _on_area_2d_body_entered(body:PhysicsBody2D):
 	player = body
 	chase = true
+	#state_machine.travel("Walk")
