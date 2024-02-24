db7b18f63d593e05e61d7fe54cbb018248d483ae
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2024-02-24 17:32:30 +0000

    The portal works just need to set conditions for it to spawn in the intended locations

--- Portal.gd
+++ Portal.gd
@@ -1,27 +1,30 @@
 extends Area2D
 
-@onready var animation_tree = $AnimationTree
+#@onready var animation_tree = $AnimationTree
 #@onready var state_machine = animation_tree.get("parameters/playback")
 
 var entered = false
 var complete = true
 
-func _on_Area2D_body_entered(body:PhysicsBody2D):
+
+func _on_body_entered(body:PhysicsBody2D):
+	print("Entered")
 	entered = true
-	
-func _on_Area2D_body_exited(body):
+
+func _on_body_exited(body):
+	print("Exit")
 	entered = false
 
 func _process(delta):
 	#if complete == true:
 		#state_machine.travel("Arriving")
 		if entered == true:
-			if Input.is_action_just_pressed("interact"):
+			if Input.is_action_just_pressed("use"):
 				print("I READ THE FUCKING INPUT DIPSHIT")
 				#get_tree().change_scene("res://Level/game_level.tscn")
 
 func condition_met():
-	
 	var complete = true
 	if complete == true:
-		animation_tree.set("parameters/Arriving/blend_position", complete)
+		print("Oh the condition thing works")
+		#animation_tree.set("parameters/Arriving/blend_position", complete)
