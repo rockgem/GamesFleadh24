ebefb805c4ec588fcc6cc66dbdc5575503c3d182
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2024-04-24 08:56:28 +0800

    dashing using "SPACE"

--- actors/entities/Player.gd
+++ actors/entities/Player.gd
@@ -6,17 +6,34 @@ var exp = 0.0
 var exp_max = 30.0
 
 var move_speed = 100.0
+var dash_speed = 140.0
+var dash_duration = 0.3
 
+var is_dashing = false
+var direction = Vector2.ZERO
 
 func _ready():
 	ManagerGame.global_player_ref = self
 
 
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("dash"):
+		dash()
+
+
 func _physics_process(delta):
+	if is_dashing:
+		velocity = direction * dash_speed * delta
+		move_and_slide()
+		
+		return
+	
 	velocity.x = Input.get_axis('left', 'right') * move_speed
 	velocity.y = Input.get_axis('up', 'down') * move_speed
 	
 	if velocity != Vector2.ZERO:
+		direction = velocity
+		
 		if velocity.x < -0.5:
 			$Sprite2D.flip_h = true
 		else:
@@ -29,6 +46,14 @@ func _physics_process(delta):
 
 func death():
 	ManagerGame.game_over.emit()
+
+
+func dash():
+	is_dashing = true
+	
+	await get_tree().create_timer(dash_duration).timeout
+	
+	is_dashing = false
 
 
 func _on_hurtbox_area_entered(area):
