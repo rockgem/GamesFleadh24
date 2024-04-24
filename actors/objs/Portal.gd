f520df07ed4d601afd54bf79f8ac412ada65683f
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2024-04-24 09:15:49 +0800

    portal pressing F to teleport

--- actors/objs/Portal.gd
+++ actors/objs/Portal.gd
@@ -6,6 +6,12 @@ func _ready():
 	activate(false)
 
 
+func _unhandled_input(event):
+	if Input.is_action_just_pressed('interact'):
+		if $Area2D.has_overlapping_bodies():
+			ManagerGame.change_scene("res://Level/Level2.tscn")
+
+
 func activate(b = true):
 	if b:
 		modulate.a = 0.0
@@ -20,4 +26,9 @@ func activate(b = true):
 
 
 func _on_area_2d_body_entered(body):
-	ManagerGame.change_scene("res://Level/Level2.tscn")
+#	ManagerGame.change_scene("res://Level/Level2.tscn")
+	$Label.show()
+
+
+func _on_area_2d_body_exited(body):
+	$Label.hide()
